3d82d984386824804fca7c1805bd00015cea44ca
evaldoe397 <evaldoe397@gmail.com>
2025-02-05 21:47:00 -0300

    correcao da formula no contractService no paypalService

--- src/model/service/ContractService.java
+++ src/model/service/ContractService.java
@@ -15,16 +15,16 @@ public class ContractService {
 	DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
 	public void processContract(Contract obj, int months) {
-		OnlinePaymentService servico = new PaypalService();
-		//PaypalService pp = new PaypalService();
+		OnlinePaymentService service = new PaypalService();
 		LocalDate P = null;
 		double parc = obj.getTotalValue() / months;
-		double amount = parc + servico.paymentFee(parc);
+		double amount = parc + service.paymentFee(parc);
 		for (int i=0; i<months; i++ ) {
 			P = obj.getLocalDate().plusMonths(i+1);
-			amount += servico.interest(amount, months);
+			amount = service.interest(amount, months);
 			Installment installment = new Installment(P,amount);
 			obj.addInstallment(installment);
+					
 		}
 	}
 }	
--- src/model/service/PaypalService.java
+++ src/model/service/PaypalService.java
@@ -4,19 +4,17 @@ public class PaypalService implements OnlinePaymentService {
 	
 	double paymentFee, interest = 0.0;
 	
+	public double interest(Double amount, Integer months) {
+		interest = amount + amount*0.01;
+		return interest;
+	}
+		
+	
 	public double paymentFee(Double amount) {
-		if (amount>0.0) {
-		paymentFee = amount *0.02;
-		}
+		paymentFee = amount*0.02;
 		return paymentFee;
 	}
 	
 	
-	public double interest(Double amount, Integer months) {
-		for (int i=0; i<months; i++) {
-		interest = (amount + paymentFee) * 0.01;
-		}
-		return interest;
-	}
-		
+	
 }
